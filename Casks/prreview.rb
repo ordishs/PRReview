a4361550c7169adc483620664c0950f156c96d00
Simon Ordish <simon.ordish@masagi.co.uk>
2026-05-29 15:58:39 +0200

    release: v0.1.0

--- Casks/prreview.rb
+++ Casks/prreview.rb
@@ -1,6 +1,6 @@
 cask "prreview" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "e6befa72904a49bbb6317f38b44e18f9104352128d2c033b4c95cddea79ef652"
 
   url "https://github.com/ordishs/code-reviewer/releases/download/v#{version}/PRReview-#{version}.dmg"
   name "PR Review"
